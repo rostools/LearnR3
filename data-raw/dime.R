48d4c37a70bb160fe672bb9025212038054cfbe7
Kaja Madsen <kaja.madsen@jubii.dk>
2025-05-06 13:48:40 +0200

    Made the first function

--- data-raw/dime.R
+++ data-raw/dime.R
@@ -5,7 +5,7 @@ dime_link <- "https://github.com/rostools/r-cubed-intermediate/raw/refs/heads/ma
 download.file(dime_link, destfile = here("data-raw/dime.zip"))
 
 # Remove leftover folder so unzipping is always clean
-dir_delete(here("data-raw/dime"))
+#dir_delete(here("data-raw/dime"))
 
 unzip(
   here("data-raw/dime.zip"),
