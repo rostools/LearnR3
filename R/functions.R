787083bae4f53339648058628b12f379cf1ac068
Luke W. Johnston <lwjohnst@gmail.com>
2026-05-05 16:23:30 +0200

    used source and functions.R

---
+++ R/functions.R
@@ -0,0 +1,16 @@
+#' Read in one nurses' stress data file.
+#'
+#' @param file_path Path to the data file.
+#' @param max_rows Maximum number of rows to read.
+#'
+#' @returns Outputs a data frame/tibble.
+#'
+read <- function(file_path, max_rows = 100) {
+  data <- file_path |>
+    readr::read_csv(
+      show_col_types = FALSE,
+      name_repair = snakecase::to_snake_case,
+      n_max = max_rows
+    )
+  return(data)
+}
