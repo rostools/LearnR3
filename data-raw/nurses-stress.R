1a4079e25505b45a0123b46bd03d65c956d4d32b
Melina Vejlø <melinanielsen2@gmail.com>
2026-05-05 12:02:38 +0200

    imported data and slightly preprosessed it

--- data-raw/nurses-stress.R
+++ data-raw/nurses-stress.R
@@ -1,4 +1,10 @@
+library(here)
+# The dataset can be downloaded manually from
+# <https://drive.proton.me/urls/ZHZ6TCAN2W#e5D5U4IajdYp>.
+# Make sure to save this file to the `data-raw/` folder with the name
+# `nurses-stress.tar`.
+
 untar(
-  "data-raw/nurses-stress.tar",
-  exdir = "data-raw/nurses-stress/"
+  here("data-raw/nurses-stress.tar"),
+  exdir = here("data-raw/nurses-stress/")
 )
