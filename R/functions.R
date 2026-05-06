7ea062a4eb813193b9c36ff1f6d7c631273e3a37
Daniel Skak Mazhari-Jensen <danmaz94@gmail.com>
2026-05-06 10:19:46 +0200

    functional session done

--- R/functions.R
+++ R/functions.R
@@ -14,3 +14,22 @@ read <- function(file_path, max_rows = 10) {
     )
   return(data)
 }
+
+
+#' Read in one nurses' stress data file.
+#'
+#' @param file_path Path to the data file.
+#' @param max_rows Maximum number of rows to read.
+#'
+#' @returns Outputs a data frame/tibble.
+#'
+read_all <- function(filename) {
+  files <- here::here("data-raw/nurses-stress/") |>
+    fs::dir_ls(regexp = filename, recurse = TRUE)
+
+  data <- files |>
+    purrr::map(read) |>
+    purrr::list_rbind(names_to = "file_path_id")
+
+  return(data)
+}
